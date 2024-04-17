2ee8e875fa18de0700ae2977e1f0dc3bb5725d92
rakesh-raya <60667540+rakesh-raya@users.noreply.github.com>
2024-04-17 16:58:26 -0500

    Pushing the Dogs Management Application

---
+++ DogsManagementSystem/src/main/java/com/example/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,16 @@
+package com.example.DMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+@author Rakesh Somanabaoina, s566617
+*/
+
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
